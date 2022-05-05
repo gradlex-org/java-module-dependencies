b3350dd1d15391f868fa641827cb1b1b3d792467
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-05-05 21:20:28 +0200

    Publish build scans / update GH actions config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,9 @@
 pluginManagement {
     includeBuild("gradle-build-logic")
 }
+plugins {
+    id("com.gradle.enterprise") version "3.10"
+}
 
 dependencyResolutionManagement {
     repositories.mavenCentral()
@@ -8,3 +11,11 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "java-module-dependencies"
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
