fbfbe6ed6f5b63b82661b1eff7eeec47901ab491
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-06-02 12:12:26 +0200

    Add more descriptive metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,19 +16,50 @@ dependencies {
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }
 
+val pluginId = "de.jjohannes.java-module-dependencies"
+val pluginClass = "de.jjohannes.gradle.moduledependencies.JavaModuleDependenciesPlugin"
+val pluginName = "Java Module Dependencies Gradle Plugin"
+val pluginDescription = "A plugin that makes Gradle respect the dependencies defined in 'module-info.java' files."
+val pluginBundleTags = listOf("java", "modularity", "jigsaw", "jpms", "dependencies", "versions")
+val pluginGitHub = "https://github.com/jjohannes/java-module-dependencies"
+
 gradlePlugin {
     plugins {
         create(project.name) {
-            id = "de.jjohannes.java-module-dependencies"
-            implementationClass = "de.jjohannes.gradle.moduledependencies.JavaModuleDependenciesPlugin"
-            displayName = "Java Module Dependencies"
-            description = "A plugin that makes Gradle respect the dependencies defined in 'module-info.java' files."
+            id = pluginId
+            implementationClass = pluginClass
+            displayName = pluginName
+            description = pluginDescription
         }
     }
 }
 
 pluginBundle {
-    website = "https://github.com/jjohannes/java-module-dependencies"
-    vcsUrl = "https://github.com/jjohannes/java-module-dependencies.git"
-    tags = listOf("java", "modularity", "jigsaw", "jpms", "dependencies", "versions")
+    website = pluginGitHub
+    vcsUrl = pluginGitHub
+    tags = pluginBundleTags
+}
+
+publishing {
+    publications.withType<MavenPublication>().all {
+        pom.name.set(pluginName)
+        pom.description.set(pluginDescription)
+        pom.url.set(pluginGitHub)
+        pom.licenses {
+            license {
+                name.set("Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        pom.developers {
+            developer {
+                id.set("jjohannes")
+                name.set("Jendrik Johannes")
+                email.set("jendrik@onepiece.software")
+            }
+        }
+        pom.scm {
+            url.set(pluginGitHub)
+        }
+    }
 }
