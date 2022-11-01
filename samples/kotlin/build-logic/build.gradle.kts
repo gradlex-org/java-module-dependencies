e02dd1e7fc6e35d9b1b0a7e400d403d885a86509
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-11-01 09:40:54 +0100

    Increase version to 1.1

--- samples/kotlin/build-logic/build.gradle.kts
+++ samples/kotlin/build-logic/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("org.gradlex:java-module-dependencies:1.0")
+    implementation("org.gradlex:java-module-dependencies:1.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 }
