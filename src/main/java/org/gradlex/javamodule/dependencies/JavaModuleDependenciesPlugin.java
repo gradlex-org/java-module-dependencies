8bc1522df782828d5dca3adb07e4daa8e74f0508
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-06-30 13:11:33 +0200

    Do not report catalog version notation if catalog is turned off

--- src/main/java/org/gradlex/javamodule/dependencies/JavaModuleDependenciesPlugin.java
+++ src/main/java/org/gradlex/javamodule/dependencies/JavaModuleDependenciesPlugin.java
@@ -159,7 +159,6 @@ public abstract class JavaModuleDependenciesPlugin implements Plugin<Project> {
 
     private void setupReportTasks(Project project, JavaModuleDependenciesExtension javaModuleDependencies) {
         SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
-        boolean usesVersionCatalog = project.getExtensions().findByType(VersionCatalogsExtension.class) != null;
         project.getTasks().register("analyzeModulePath", ModulePathAnalysis.class, t -> {
             t.setGroup(HELP_GROUP);
             t.setDescription("Check consistency of the Module Path");
@@ -173,8 +172,8 @@ public abstract class JavaModuleDependenciesPlugin implements Plugin<Project> {
             t.setGroup(HELP_GROUP);
             t.setDescription("Query repositories for latest stable versions of the used Java Modules");
 
-            t.getPrintForPlatform().convention(!usesVersionCatalog);
-            t.getPrintForCatalog().convention(usesVersionCatalog);
+            t.getPrintForPlatform().convention(javaModuleDependencies.getWarnForMissingVersions().map(b -> !b));
+            t.getPrintForCatalog().convention(javaModuleDependencies.getWarnForMissingVersions());
         });
     }
 
