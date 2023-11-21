13153d8bfe7997e2c2fe2110de43eeaad42975f7
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-11-21 13:46:01 +0100

    Make 'extra-module-info' bridge more stable

--- src/main/java/org/gradlex/javamodule/dependencies/internal/bridges/ExtraJavaModuleInfoBridge.java
+++ src/main/java/org/gradlex/javamodule/dependencies/internal/bridges/ExtraJavaModuleInfoBridge.java
@@ -16,9 +16,10 @@
 
 package org.gradlex.javamodule.dependencies.internal.bridges;
 
-import org.gradlex.javamodule.moduleinfo.ExtraJavaModuleInfoPluginExtension;
-import org.gradlex.javamodule.dependencies.JavaModuleDependenciesExtension;
 import org.gradle.api.Project;
+import org.gradlex.javamodule.dependencies.JavaModuleDependenciesExtension;
+import org.gradlex.javamodule.moduleinfo.ExtraJavaModuleInfoPluginExtension;
+import org.gradlex.javamodule.moduleinfo.ModuleSpec;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -28,6 +29,15 @@ public class ExtraJavaModuleInfoBridge {
     public static void autoRegisterPatchedModuleMappings(Project project, JavaModuleDependenciesExtension javaModuleDependencies) {
         ExtraJavaModuleInfoPluginExtension extraJavaModuleInfo = project.getExtensions().getByType(ExtraJavaModuleInfoPluginExtension.class);
         javaModuleDependencies.getModuleNameToGA().putAll(extraJavaModuleInfo.getModuleSpecs().map(
-                moduleSpecs -> moduleSpecs.entrySet().stream().collect(Collectors.toMap(e -> e.getValue().getModuleName(), Map.Entry::getKey))));
+                moduleSpecs -> moduleSpecs.entrySet().stream().collect(Collectors.toMap(ExtraJavaModuleInfoBridge::moduleNameKey, Map.Entry::getKey))));
+    }
+
+    private static String moduleNameKey(Map.Entry<String, ModuleSpec> entry) {
+        if (!entry.getKey().contains(":")) {
+            // Entry is not usable as mapping (e.g., because it uses file names instead of GA coordinates).
+            // Invalidate this mapping entry by creating a key that is not a valid module name.
+            return "__" + entry.getValue().getModuleName();
+        }
+        return entry.getValue().getModuleName();
     }
 }
