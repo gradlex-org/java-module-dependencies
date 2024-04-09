3cd604bc52040b90aa303002100d92cdc9ad34b3
Benedikt Ritter <beneritter@gmail.com>
2024-04-09 10:51:44 +0200

    Migrate to develocity plugin ID

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ pluginManagement {
     includeBuild("gradle/plugins")
 }
 plugins {
-    id("com.gradle.enterprise") version "3.17"
+    id("com.gradle.develocity") version "3.17"
 }
 
 dependencyResolutionManagement {
