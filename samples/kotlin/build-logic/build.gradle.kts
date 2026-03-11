a87c5ca9c7e16f1eb733c72055ebb4201e9922a9
Jendrik Johannes <jendrik@onepiece.software>
2026-03-11 16:42:13 +0100

    Version 1.12.1

--- samples/kotlin/build-logic/build.gradle.kts
+++ samples/kotlin/build-logic/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("org.gradlex:java-module-dependencies:1.12")
+    implementation("org.gradlex:java-module-dependencies:1.12.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.10")
 }
